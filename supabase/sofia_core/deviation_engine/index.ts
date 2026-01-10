d8c236eea30985da4b7b2419c042258af3b0d3e6
EmeraldOrbit <contact@168emeraldestatesllc.com>
2026-01-10 13:25:32 -0800

    Update index.ts

--- supabase/sofia_core/deviation_engine/index.ts
+++ supabase/sofia_core/deviation_engine/index.ts
@@ -1,1 +1,11 @@
-export * from "./src/deviation_engine";
+import { deviationEngine } from "./deviation_engine/src/deviation_engine";
+import { identityFilter } from "./identity_filter/src/identity_filter";
+import { membraneEngine } from "./membrane_engine/src/membrane_engine";
+import { tonalEngine } from "./tonal_engine/src/tonal_engine";
+
+export const sofiaCoreIndex = {
+  deviation_engine: deviationEngine,
+  identity_filter: identityFilter,
+  membrane_engine: membraneEngine,
+  tonal_engine: tonalEngine
+};
